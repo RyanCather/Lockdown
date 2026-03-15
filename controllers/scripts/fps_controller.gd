cd01cad4259077cb5017ea5f8bfe04213d911d58
Thane1719 <9937214@schoolsnet.act.edu.au>
2026-03-16 10:22:23 +1100

    Spawn

--- controllers/scripts/fps_controller.gd
+++ controllers/scripts/fps_controller.gd
@@ -1,10 +1,11 @@
-class_name Player 
+class_name Player
 extends CharacterBody3D
 
 @export var JUMP_VELOCITY : float = 4.5
 @export var MOUSE_SENSITIVITY : float = 0.5
 @export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
 @export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
+
 @onready var CAMERA_CONTROLLER : Camera3D = $CameraController/Camera3D
 @onready var ANIMATIONPLAYER : AnimationPlayer = $AnimationPlayer
 @onready var CROUCH_SHAPECAST : Node3D = %ShapeCast3D
@@ -25,95 +26,112 @@ var isCrouching : bool = false
 var currentRotation : float
 var cameraOffset : Vector3
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var gravity = 12
 var stamina = 100
+
 
 func _enter_tree():
 	print(name)
 	set_multiplayer_authority(str(name).to_int())
 
 
+func _ready():
+
+	if not is_multiplayer_authority():
+		CAMERA_CONTROLLER.current = false
+		return
+
+	CAMERA_CONTROLLER.make_current()
+
+	velocity = Vector3.ZERO
+
+	Global.player = self
+	Global.playerCamera = CAMERA_CONTROLLER
+
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+	CROUCH_SHAPECAST.add_exception(self)
+
+
 func _unhandled_input(event: InputEvent) -> void:
-	if not is_multiplayer_authority(): return
+
+	if not is_multiplayer_authority():
+		return
+
 	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+
 	if _mouse_input:
 		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
 		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
-		
-		
+
+
 func _input(event):
-	if not is_multiplayer_authority(): return
+
+	if not is_multiplayer_authority():
+		return
+
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
 
+
 func _update_camera(delta):
-	if not is_multiplayer_authority(): return
-	# Rotates camera using euler rotation
+
+	if not is_multiplayer_authority():
+		return
+
 	_mouse_rotation.x += _tilt_input * delta
 	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
 	_mouse_rotation.y += _rotation_input * delta
-	
+
 	_player_rotation = Vector3(0.0,_mouse_rotation.y,0.0)
 	_camera_rotation = Vector3(_mouse_rotation.x,0.0,0.0)
 
 	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
 	global_transform.basis = Basis.from_euler(_player_rotation)
-	
+
 	CAMERA_CONTROLLER.rotation.z = 0.0
 
 	_rotation_input = 0.0
 	_tilt_input = 0.0
 
-func _ready():
-	if not is_multiplayer_authority(): 
-		CAMERA_CONTROLLER.current = false
-		return
-	CAMERA_CONTROLLER.make_current()
-	position = get_node("/root/World/Spawnpoint").position
-	velocity = Vector3(0, 0, 0)
-	Global.player = self
-	Global.playerCamera = CAMERA_CONTROLLER
-	# Get mouse input
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	#Prevents player from activating crouch cast
-	CROUCH_SHAPECAST.add_exception($".")
-	#THIS WILL BE ANNOUNCE NOT LEVEL CHANGE TEXT
-	#showLevelText("This is a test for if robbers got something")
 
 func _physics_process(delta):
-	if not is_multiplayer_authority(): return
-	# Update camera movement based on mouse movement
+
+	if not is_multiplayer_authority():
+		return
+
 	_update_camera(delta)
-	
+
 	Global.debug.addProperty("Speed", get_real_velocity().length(), 2)
 	Global.debug.addProperty("Stamina", stamina, 2)
+
 	if stamina < 101 and stateMachine.currentState != $PlayerStateMachine/SprintingPlayerState:
 		stamina += ceil(16.5 * delta)
+
 	playerlabelname.text = str(multiplayer.get_unique_id())
-	
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity.y -= gravity * delta
+
 	CAMERA_CONTROLLER.rotation = lerp(CAMERA_CONTROLLER.rotation, CAMERA_CONTROLLER.rotation + cameraOffset, 0.1)
-	cameraOffset = lerp(cameraOffset, Vector3(0,0,0), 0.05)
-	
-	
+	cameraOffset = lerp(cameraOffset, Vector3.ZERO, 0.05)
+
+
 func updateGravity(delta) -> void:
-	if not is_multiplayer_authority(): return
+
+	if not is_multiplayer_authority():
+		return
+
 	velocity.y -= gravity * delta
-	
+
 
 func updateInput(speed: float, acceleration: float, deceleration: float) -> void:
-	if not is_multiplayer_authority(): return
-		# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
+	if not is_multiplayer_authority():
+		return
+
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 	var weightedSpeed = clamp(speed - (weaponGlobal.inventoryWeight / 2), 0.5, 10000)
-	
+
 	if direction:
 		velocity.x = lerp(velocity.x,direction.x * weightedSpeed, acceleration)
 		velocity.z = lerp(velocity.z,direction.z * weightedSpeed, acceleration)
@@ -121,24 +139,22 @@ func updateInput(speed: float, acceleration: float, deceleration: float) -> void
 		velocity.x = move_toward(velocity.x, 0, deceleration)
 		velocity.z = move_toward(velocity.z, 0, deceleration)
 
+
 func updateVelocity() -> void:
-	if not is_multiplayer_authority(): return
+
+	if not is_multiplayer_authority():
+		return
+
 	move_and_slide()
+
 
 @rpc("any_peer")
 func take_damage(damage, type, team):
+
 	if team != Global.myCurrentTeam:
+
 		Global.playerHealth -= damage
 		Global.updateHealth()
-		if Global.playerHealth <= 0:
-			position = get_node("/root/World/Spawnpoint").position
-			velocity = Vector3(0, 0, 0)
-			Global.playerHealth = 100
 
-#THIS NEEDS UPDATING TO NEW UI PLEASE
-#WILL BE ANNOUNCEMENT TEXT NOT LEVEL CHANGE
-#func showLevelText(spawnText):
-	#%"Spawn Label".text = spawnText
-	#animationPlayer.play("Level Fade", -1, 1, false)
-	#await animationPlayer.animation_finished
-	#animationPlayer.play("Level Fade", -1, -1, true)
+		if Global.playerHealth <= 0:
+			Global.playerHealth = 100
--- scenes/world.gd
+++ scenes/world.gd
@@ -4,56 +4,23 @@ extends Node
 @onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
 @onready var hud = $UserInterface
 
-
 @onready var Player = preload("res://controllers/fps_controller.tscn")
-#@onready var Player = $Player
+
+@onready var cop_spawns = $SpawnPoints2/Cops.get_children()
+@onready var robber_spawns = $SpawnPoints2/Robber.get_children()
+
 var tracked = false
 var player
-var teams = {} # peer_id -> "Cop" or "Robber"
-
+var teams = {}
 
 const PORT = 9999
 var enet_peer = ENetMultiplayerPeer.new()
 
 
-func _on_host_button_pressed():
-	main_menu.hide()
-	hud.show()
-	
-	enet_peer.create_server(PORT)
-	multiplayer.multiplayer_peer = enet_peer
-	multiplayer.peer_connected.connect(add_player)
-	multiplayer.peer_disconnected.connect(remove_player)
-	
-	add_player(multiplayer.get_unique_id())
-	
-	#upnp_setup()
-func _on_join_button_pressed():
-	main_menu.hide()
-	hud.show()
-	
-	enet_peer.create_client(address_entry.text, PORT)
-	multiplayer.multiplayer_peer = enet_peer
+func _ready():
 
-#func _on_multiplayer_spawner_spawned(node):
-	#if node.is_multiplayer_authority():
-		#node.health_changed.connect(update_health_bar)
-func upnp_setup():
-	var upnp = UPNP.new()
-	
-	var discover_result = upnp.discover()
-	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP Discover Failed! Error %s" % discover_result)
-
-	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), "UPNP Invalid Gateway!")
-
-	var map_result = upnp.add_port_mapping(PORT)
-	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP Port Mapping Failed! Error %s" % map_result)
-	
-	print("Success! Join Address: %s" % upnp.query_external_address())
-
-
-func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
 	Global.reserveLabel = %Reserve
 	Global.interactionLabel = %InteractionLabel
 	Global.clipLabel = %Clip
@@ -61,73 +28,115 @@ func _ready() -> void:
 	Global.healthLabel = %Health
 
 
-func _physics_process(delta):
-	pass
-	#if tracked:
-		#get_tree().call_group("enemy", "update_target_location", player.global_transform.origin)
+func _on_host_button_pressed():
 
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()
-
-func _on_single_player_button_pressed():
 	main_menu.hide()
 	hud.show()
+
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+
+	add_player(multiplayer.get_unique_id())
+
+
+func _on_join_button_pressed():
+
+	main_menu.hide()
+	hud.show()
+
+	enet_peer.create_client(address_entry.text, PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+
+func _on_single_player_button_pressed():
+
+	main_menu.hide()
+	hud.show()
+
 	var my_id = multiplayer.get_unique_id()
-	#multiplayer.multiplayer_peer = enet_peer
 	add_player(my_id)
-	
 
 
 func add_player(peer_id):
-	player = Player.instantiate()
+
+	var player = Player.instantiate()
 	player.name = str(peer_id)
+
 	add_child(player)
+
+	player.set_multiplayer_authority(peer_id)
+
 	assign_team(peer_id)
+
 	tracked = true
-	#if player.is_multiplayer_authority():
-		#player.health_changed.connect(update_health_bar)
+
 
 func remove_player(peer_id):
+
 	var player = get_node_or_null(str(peer_id))
+
 	if player:
 		player.queue_free()
 
+
 func assign_team(id):
+
 	if !multiplayer.is_server():
 		return
-		
+
 	var cop_count = 0
 	var robber_count = 0
-	
+
 	for t in teams.values():
+
 		if t == "Cop":
 			cop_count += 1
 		elif t == "Robber":
 			robber_count += 1
 
+
 	var team
-	# Keep teams balanced first
+
 	if cop_count > robber_count:
 		team = "Robber"
 	elif robber_count > cop_count:
 		team = "Cop"
 	else:
-		# If equal, randomly assign
 		team = "Cop" if randi() % 2 == 0 else "Robber"
+
+
 	teams[id] = team
+
 	print("Player ", id, " assigned to ", team)
-	if id == multiplayer.get_unique_id():
-		Global.myCurrentTeam = team
+
 	rpc("receive_team_assignment", id, team)
-	
+
+	spawn_player(id, team)
+
+
+func spawn_player(id, team):
+
+	var player = get_node(str(id))
+
+	var spawn_point
+
+	if team == "Cop":
+		spawn_point = cop_spawns.pick_random()
+	else:
+		spawn_point = robber_spawns.pick_random()
+
+	player.global_position = spawn_point.global_position
 
 
 @rpc("any_peer", "reliable")
 func receive_team_assignment(id, team):
+
 	teams[id] = team
+
 	print("Synced: Player ", id, " is ", team)
-	# If this is me, confirm team locally
+
 	if id == multiplayer.get_unique_id():
-		print("I am on team: ", team)
 		Global.myCurrentTeam = team
